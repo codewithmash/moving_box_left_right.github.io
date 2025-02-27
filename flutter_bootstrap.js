41ed1174d3df3920a6672197c37889701ba8b765
unknown <codewithmash@gmail.com>
2025-02-27 23:11:37 +0530

    moving box anim

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2407497627"
+    serviceWorkerVersion: "3734772378"
   }
 });
